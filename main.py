0f3a1064ca9f7a08658b2553aa61b2ec56afaa9f
Martijn Stommels <martijn@martijnpc.nl>
2019-12-16 14:56:09 +0100

    En/disable remote repl via MQTT

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import machine
 import esp
 import mqtt
 import ujson
-import influxdb
+from influxdb import Measurement
 
 config = ujson.loads(open('config.json').read())
 
@@ -18,6 +18,14 @@ led = Signal(ledPin, invert=True)
 
 sta_if = network.WLAN(network.STA_IF)
 
+client = mqtt.Sensor(name=config['meta']['id'],
+                    server=config['mqtt']['server'],
+                    port=config['mqtt']['port'],
+                    keyfile=config['mqtt']['key_file'],
+                    certfile=config['mqtt']['cert_file'],
+                    root_topic=config['mqtt']['root_topic'])
+
+
 def togleLed():
     led.value(not led.value())
 
@@ -27,14 +35,17 @@ def scan():
         for config_network in config['networks']:
             if scanned[0].decode() == config_network[0]:
                 return (config_network[0], config_network[1])
-    return None
+    return None, None
 
 def do_conncect():
     issd, password = scan()
-    print('Connecting to:', issd)
-    sta_if.connect(issd, password) 
+    if not issd:
+        print('No networks found')
+    else:
+        print('Connecting to:', issd)
+        sta_if.connect(issd, password) 
 
-def connect():
+def connect_network():
     if not sta_if.isconnected():
         sta_if.active(True)
         do_conncect()
@@ -52,64 +63,68 @@ def connect():
     led.off()
     print('network config:', sta_if.ifconfig())
 
-    import webrepl
-    import gc
-    webrepl.start()
-    gc.collect()
-
-def send():
+def connect_mqtt():
     led.on()
-    
-    client = mqtt.Sensor(name=config['meta']['id'],
-                         server=config['mqtt']['server'],
-                         port=config['mqtt']['port'],
-                         keyfile=config['mqtt']['key_file'],
-                         certfile=config['mqtt']['cert_file'],
-                         root_topic=config['mqtt']['root_topic'])
 
     client.connect()
     print('Connected to MQTT broker')
 
-    measurement = influxdb.Measurement('climate')
-    measurement.tags = {'location' : 'woonkamer',
-                        'id': 'bureau'}
-    measurement.fields = {'temperature' : sensor.temperature(), 
-                          'humidity': sensor.humidity()}
+    if client.repl :
+        import webrepl
+        import gc
+        webrepl.start()
+        gc.collect()
+    else:
+        print('REPL disabled')
 
-    client.publish_measurment(measurement)
-
-    client.disconnect()
+    client.on_repl_disabled = on_repl_disabled
         
     led.off()
 
+def send_measurment():
+    measurement = Measurement('climate')
+    measurement.tags.update(config['meta'])
+    measurement.fields = {'temperature' : sensor.temperature(), 
+                            'humidity': sensor.humidity()}
+
+    client.publish_measurment(measurement)
+
+def on_repl_disabled():
+    print('REPL disabled via MQTT')
+    sleep()
+
+def sleep():
+    if config['deepsleep'] and not client.repl:
+            print('Entering deepsleep')
+
+            client.disconnect()
+
+            # give us some time to reprogram
+            if(machine.reset_cause() != machine.DEEPSLEEP_RESET):
+                for x in range(10):
+                    togleLed()
+                    time.sleep_ms(100)
+                    togleLed()
+                    time.sleep_ms(500)
+
+            micro = 1000000
+            esp.deepsleep(config['interval_minutes'] * 60 * micro)
+            time.sleep(1)
+    else:
+        for x in range(config['interval_minutes'] * 60):
+            client.update()
+            time.sleep(1)
 
 def main():
     try:
         network.WLAN(network.AP_IF).active(False)
-        
-        connect()
+        sensor.measure()
+        connect_network()
+        connect_mqtt()
         
         while True:
-            sensor.measure()
-            send()
-            #time.sleep(10)
-            break
-        
-        time.sleep_ms(100)
-
-        # give us some time to reprogram
-        if(machine.reset_cause() != machine.DEEPSLEEP_RESET):
-             for x in range(10):
-                togleLed()
-                time.sleep_ms(100)
-                togleLed()
-                time.sleep_ms(500)
-           
-        print('Entering deepsleep')
-        micro = 1000000
-        esp.deepsleep(5 * 60 * micro)
-
-
+            send_measurment()
+            sleep()
 
     except Exception as e:
         from sys import print_exception
--- mqtt.py
+++ mqtt.py
@@ -1,10 +1,11 @@
 from umqtt.simple import MQTTClient
-import influxdb
 
 class Sensor:
     def __init__(self, name, server, port, keyfile, certfile, root_topic):
 
         self.topic = root_topic + '/' + name
+        self.repl = None
+        self.on_repl_disabled = None
 
         with open(keyfile, 'rb') as f:
             key = f.read()
@@ -20,11 +21,15 @@ class Sensor:
                                 keepalive=60)
 
     def connect(self):
+        self.client.set_callback(self._subscribe_callback)
+        
         self.client.set_last_will(self.topic + '/status', b'offline', retain=True, qos=1)
         self.client.connect()
 
         self.publish_status(b'online')
 
+        self.client.subscribe(self.topic + '/repl')
+        self.client.wait_msg()
 
     def disconnect(self):
         self.publish_status(b'offline')
@@ -32,6 +37,10 @@ class Sensor:
 
     def publish(self, topic, data):
         self.client.publish(topic, data)
+
+    def update(self):
+        self.client.ping()
+        self.client.wait_msg()
 
     def publish_measurment(self, measurment):
         topic = self.topic + '/measurement'
@@ -42,3 +51,16 @@ class Sensor:
 
     def publish_status(self, status):
         self.client.publish(self.topic + '/status', status, retain=True, qos=1)
+
+    def _subscribe_callback(self, topic, msg):
+        print('Received: ', topic, msg)
+
+        if topic.decode() == self.topic + '/repl':
+            
+            prev = self.repl
+            self.repl = (msg == b'1')
+            
+            if self.on_repl_disabled:
+                if prev and not self.repl:
+                    self.on_repl_disabled()
+
